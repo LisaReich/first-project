17bbae66ebc931a70b62574b33c9fa48957bd821
Lisa Reich <lisa.reich@yandex.ru>
2022-06-01 15:57:21 +0300

    Мой второй коммит

--- Cinema/src/Praktikum/Praktikum.java
+++ Cinema/src/Praktikum/Praktikum.java
@@ -9,6 +9,7 @@ public class Praktikum {
         System.out.println();
         System.out.println("Сколько денег у вас осталось до зарплаты?");
         System.out.println("Сколько денег у вас осталось после зарплаты?");
+        System.out.println("Сколько денег у вас осталось после второй  зарплаты?");
         double moneyBeforeSalary = scanner.nextDouble();
 
         System.out.println("За какой день вы хотите ввести трату: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?");
